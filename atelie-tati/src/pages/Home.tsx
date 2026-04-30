d8e2e21966404bd239dd9743d724ea0805b1ad69
Eduardo Bertolin <eduardo_bertolin@hotmail.com>
2026-04-29 21:47:19 -0300

    configuração css

---
+++ atelie-tati/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import '../styles/utility.css';
+import '../styles/index.css';
+
+export default function Home() {
+    return (
+        <>
+        <main>
+            <div className="container">
+                <h1>texto</h1>
+            </div>
+        </main>
+        </>
+    )
+}
