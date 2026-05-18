97a178a3babf7c0031828f2cd0676798a39b35fb
Eduardo Bertolin <eduardo_bertolin@hotmail.com>
2026-05-18 17:56:40 -0300

    edit header & Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,58 @@
 import "../styles/utility.css";
+import Costura from "../assets/images/costura.png";
 import Header from "../components/Header";
-import Hero from "../components/Hero";
-
 export default function Home() {
+  const backgroundStyle = {
+    backgroundColor: "var(--color-ebano)",
+    backgroundImage: `repeating-linear-gradient(
+    45deg,
+    #3b2a1c,
+    transparent 1px,
+    transparent 12px
+  )`,
+    minHeight: "calc(100vh - var(--nav-height))",
+  };
   return (
     <>
       <Header />
 
       <main className="flex items-center">
-        <Hero />
+        <section id="hero">
+          <div className="flex items-center background" style={backgroundStyle}>
+            <div className="container flex hero-row justify-between">
+              <div className="hidden xl:flex flex-col gap-[3rem] self-center">
+                <div
+                  className="flex items-center gap-4"
+                >
+
+                  <span className="line" />
+
+                  <span className="font-jost color-dourado uppercase fs-md ls-md">
+                    Ateliê de Patchwork
+                  </span>
+                </div>
+                <h1 className="font-serif color-cru fs-6xl">
+                  Feito à mão, <br />
+                  com{" "}
+                  <span className="font-cormorant color-dourado fs-6xl">
+                    propósito.
+                  </span>
+                </h1>
+                <p className="font-cormorant color-cru-translucent max-w-[350px] fs-xl ">
+                  Costura artesanal para quem acredita em peças únicas.
+                </p>
+              </div>
+
+              <div className="hero-image desktop-only">
+                <img
+                  src={Costura}
+                  alt="Costura artesanal"
+                  className="w-[600px] h-[700px] object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
