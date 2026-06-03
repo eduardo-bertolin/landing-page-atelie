bbf9f7190a47107fc0b5d5a6cb1b9802bad7e4ed
eduardo-bertolin <eduardo_bertolin@hotmail.com>
2026-06-03 14:58:45 -0300

    a

--- src/components/Avaliacoes.tsx
+++ src/components/Avaliacoes.tsx
@@ -4,7 +4,7 @@ export default function Avaliacoes() {
     return (
         <div className="container flex flex-col xl:flex-row h-auto gap-1 w-full">
             <div className="bg-red-500 h-60 w-full xl:w-1/3">
-                <GridAvaliacoes stars={5} comment={`"piru enorme muito grande pra caralho"`} name="Eduardo Bertolin" profession="Cliente" />
+                <GridAvaliacoes stars={5} comment={`"TESTE TESTE TESTE KANYE WEST"`} name="Eduardo Bertolin" profession="Cliente" />
 
             </div>
         </div>
